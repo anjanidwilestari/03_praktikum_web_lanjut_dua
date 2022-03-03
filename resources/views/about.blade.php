dbb3aeaeb13ff28819f6c59dc42092f9f5375477
Ayi <ayianjani774@gmail.com>
2022-03-03 20:36:27 +0700

    edit kode kedua

---
+++ resources/views/about.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('tittle')
+    {{ $title }}
+@endsection
+
+@section('statusHome')
+    {{ $status }}    
+@endsection
+
+@section('content')
+<div class="page-h promotion">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h3> ABOUT US </h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="fourth-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2>SEJARAH EDUCA STUDIO MODIFY</h2>
+                <p>Educa Studio - Free Education Games & High Quality Casual Games </p>
+                <p>Produce many kinds of Free Education Games, High Quality Casual Games. </p>
+                <p> Also produce business applications, website and mobile applications.</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
