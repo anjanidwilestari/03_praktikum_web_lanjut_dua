87be4c1b68d9f8536c2e2f71bdbfcda86970878d
Ayi <ayianjani774@gmail.com>
2022-03-07 08:28:06 +0700

    edit delete status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,12 +32,12 @@
                     </div>
                     <div class="collapse navbar-collapse" id="main-menu">
                         <ul class="nav navbar-nav navbar-right">
-                            <li ><a href="/home">Home</a></li>
-                            <li ><a href="/product">Products</a></li>
-                            <li ><a href="/program">Program</a></li>
-                            <li ><a href="/news">News</a></li>
-                            <li ><a href="/about">About</a></li>
-                            <li ><a href="/contact">Contact</a></li>
+                            <li><a href="/home">Home</a></li>
+                            <li><a href="/product">Products</a></li>
+                            <li><a href="/program">Program</a></li>
+                            <li><a href="/news">News</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/contact">Contact</a></li>
                         </ul>
                     </div>
                 </nav>         
